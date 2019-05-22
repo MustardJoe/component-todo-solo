726b2eeedc7806de1c930e28b21a9b008a4de34e
Jonathan Forney <jonathan.forney@gmail.com>
2019-05-22 10:50:22 -0700

    peiced together enough stuff - the easey stuff- so that header function goes through app function and populates on html, just to know basic stuff up and running

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import Component from './Component.js';
+import Header from './Header.js';
+import todos from '../todo-data.js';
+
+class App extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const header = new Header;
+        const headerDOM = header.render();
+        const main = dom.querySelector('main');
+        dom.insertBefore(headerDOM, main);
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <!-- Header goes here -->
+                <main>
+                    <!-- CatList goes here -->
+                </main>
+            </div>
+        `;
+    }
+    
+}
+
+export default App;
